16db2cec2083decab759f0093dc46f02d69a6679
kaykyjuvenal <neguito.juvenal@gmail.com>
2026-03-17 10:07:24 -0300

    feat: Add Fallback implemantation for StarWarsService

--- src/main/java/org/juvenal/flavio/kayky/StarWarsService.java
+++ src/main/java/org/juvenal/flavio/kayky/StarWarsService.java
@@ -5,6 +5,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "https://swapi.info/api/")
@@ -12,5 +14,13 @@ public interface StarWarsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("starships")
+    @Timeout(value = 1000L)
+    @Fallback(
+            fallbackMethod = "getStarshipsFallback "
+    )
     public String getStarships();
+    default String getStarshipsFallback(){
+        return "Fallback";
+    };
+
 }
