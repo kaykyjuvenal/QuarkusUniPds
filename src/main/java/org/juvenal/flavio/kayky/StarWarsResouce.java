730bebedb03e3bd1a6b36b1be3ae6a730822c561
kaykyjuvenal <neguito.juvenal@gmail.com>
2026-03-17 09:36:24 -0300

    fix: Renamed StarWarsResouce classname

--- src/main/java/org/juvenal/flavio/kayky/StarWarsResouce.java
+++ src/main/java/org/juvenal/flavio/kayky/StarWarsResouce.java
@@ -3,15 +3,18 @@ package org.juvenal.flavio.kayky;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("starwars")
-public class StarsWarsResouce{
+public class StarWarsResouce{
     @RestClient
     StarWarsService starWarsService;
 
     @GET
     @Path("starships")
+    @Produces(MediaType.APPLICATION_JSON)
     public String getStarships() {
         return starWarsService.getStarships();
     }
